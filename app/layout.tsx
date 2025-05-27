b0ecf9c699dca3bf38b45399316f0035fb07ccfe
Thimira Navodana <thimiranavodana2002@gmail.com>
2025-05-27 18:32:34 +0530

    code update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,6 @@ import type { Metadata } from 'next';
 import { Inter, JetBrains_Mono } from 'next/font/google';
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
-// import { Header } from '@/components/layout/header';
-// import { Sidebar } from '@/components/layout/sidebar';
-// import AuthGuard from '@/app/login/authguard';
-// import { usePathname } from 'next/navigation';
 import AuthGuardWrapper from "@/app/login/authguardwrapper";
 import { Analytics } from '@vercel/analytics/next';
 
@@ -14,16 +10,12 @@ const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 const jetbrainsMono = JetBrains_Mono({ subsets: ['latin'], variable: '--font-mono' });
 
 export const metadata: Metadata = {
-  title: 'NeoMetropolis Security Response System',
+  title: 'NeoMetropolis Cyber Security Response System',
   description: 'Cyber Security Response System for NeoMetropolis',
 };
 
 // Marking this as a client component to use usePathname
-export default function RootLayout({
-                                     children,
-                                   }: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children, }: { children: React.ReactNode; }) {
   return (
       <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} ${jetbrainsMono.variable} font-sans`}>
